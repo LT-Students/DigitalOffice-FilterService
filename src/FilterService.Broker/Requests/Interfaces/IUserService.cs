eb08e95d3c9a4caa881406682058edafdea5c9e4
Misha <vasiljev.mish90@gmail.com>
2022-04-07 11:12:22 +0300

    add pagination and redis

---
+++ src/FilterService.Broker/Requests/Interfaces/IUserService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using LT.DigitalOffice.FilterService.Models.Dto.Request.UserService;
+using LT.DigitalOffice.Kernel.Attributes;
+using LT.DigitalOffice.Models.Broker.Models;
+
+namespace LT.DigitalOffice.FilterService.Broker.Requests.Interfaces
+{
+  [AutoInject]
+  public interface IUserService
+  {
+    Task<List<UserData>> GetUsersDataAsync(
+      List<Guid> usersIds,
+      UserFilter filter,
+      PaginationValues value,
+      List<string> errors);
+  }
+}
---
+++ src/FilterService.Broker/Requests/UserService.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using LT.DigitalOffice.FilterService.Broker.Requests.Interfaces;
+using LT.DigitalOffice.FilterService.Models.Dto.Request.UserService;
+using LT.DigitalOffice.Kernel.BrokerSupport.Helpers;
+using LT.DigitalOffice.Kernel.RedisSupport.Constants;
+using LT.DigitalOffice.Kernel.RedisSupport.Extensions;
+using LT.DigitalOffice.Kernel.RedisSupport.Helpers.Interfaces;
+using LT.DigitalOffice.Models.Broker.Models;
+using LT.DigitalOffice.Models.Broker.Requests.User;
+using LT.DigitalOffice.Models.Broker.Responses.User;
+using MassTransit;
+using Microsoft.Extensions.Logging;
+
+namespace LT.DigitalOffice.FilterService.Broker.Requests
+{
+  public class UserService : IUserService
+  {
+    private readonly ILogger<UserService> _logger;
+    private readonly IGlobalCacheRepository _globalCache;
+    private readonly IRequestClient<IGetUsersDataRequest> _rcGetUsers;
+
+    public UserService(
+      ILogger<UserService> logger,
+      IGlobalCacheRepository globalCache,
+      IRequestClient<IGetUsersDataRequest> rcGetUsers)
+    {
+      _logger = logger;
+      _globalCache = globalCache;
+      _rcGetUsers = rcGetUsers;
+    }
+
+    public async Task<List<UserData>> GetUsersDataAsync(
+      List<Guid> usersIds,
+      UserFilter filter,
+      PaginationValues value,
+      List<string> errors)
+    {
+      if (filter.DepartmentsIds is null &&
+          filter.PositionsIds is null &&
+          filter.RolesIds is null &&
+          filter.OfficesIds is null)
+      {
+        return
+          (await RequestHandler.ProcessRequest<IGetUsersDataRequest, IGetUsersDataResponse>(
+            _rcGetUsers,
+            IGetUsersDataRequest.CreateObj(new List<Guid>(), value.SkipCount, value.TakeCount),
+            errors,
+            _logger))
+          .UsersData;
+      }
+
+      List<UserData> usersData = await _globalCache.GetAsync<List<UserData>>(Cache.Users, usersIds.GetRedisCacheHashCode());
+      if (usersData is null)
+      {
+        usersData =
+          (await RequestHandler.ProcessRequest<IGetUsersDataRequest, IGetUsersDataResponse>(
+            _rcGetUsers,
+            IGetUsersDataRequest.CreateObj(usersIds, value.SkipCount, value.TakeCount),
+            errors,
+            _logger))
+          .UsersData;
+      }
+
+      return usersData;
+    }
+  }
+}
