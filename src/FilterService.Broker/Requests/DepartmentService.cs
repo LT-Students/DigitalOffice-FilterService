13655af10d2a07595199e4760450c9c918243010
Vladimir Liamin <LyaminVA@yandex.ru>
2022-07-16 00:19:39 +0300

    add empty line

--- src/FilterService.Broker/Requests/DepartmentService.cs
+++ src/FilterService.Broker/Requests/DepartmentService.cs
@@ -53,6 +53,7 @@ namespace LT.DigitalOffice.FilterService.Broker.Requests
             _logger))
           ?.Departments;
       }
+
       if (departmentsData is null)
       {
         errors.Add("Can not filter by departments.");
